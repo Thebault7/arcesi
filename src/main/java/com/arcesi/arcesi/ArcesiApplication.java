c4ac2dd1e0df23ada95ac3ab4123981d86c16135
Thebault7 <frederic.thebault35@gmail.com>
2021-07-24 01:29:31 +0200

    third commit

--- src/main/java/com/arcesi/arcesi/ArcesiApplication.java
+++ src/main/java/com/arcesi/arcesi/ArcesiApplication.java
@@ -2,20 +2,11 @@ package com.arcesi.arcesi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class ArcesiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ArcesiApplication.class, args);
-	}
-}
-
-@RestController
-class HelloController {
-	
-	String hello() {
-		return "Salut tout le monde !!!";
 	}
 }
