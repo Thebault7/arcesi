b8abbb5030834963b8fe849a664203bf9388228c
Thebault7 <frederic.thebault35@gmail.com>
2021-07-27 17:05:32 +0200

    third commit

--- src/main/java/com/arcesi/arcesi/model/Person.java
+++ src/main/java/com/arcesi/arcesi/model/Person.java
@@ -2,23 +2,42 @@ package com.arcesi.arcesi.model;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table
 public class Person implements Serializable {
 	
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
 	private String firstName;
     private String lastName;
  
     public Person() {
- 
+    	super();
     }
  
     public Person(String firstName, String lastName) {
         this.firstName = firstName;
         this.lastName = lastName;
     }
+    
+    public Long getId() {
+		return id;
+	}
+    
+    public void setId(Long id) {
+		this.id = id;
+	}
  
     public String getFirstName() {
         return firstName;
@@ -35,4 +54,9 @@ public class Person implements Serializable {
     public void setLastName(String lastName) {
         this.lastName = lastName;
     }
+
+	@Override
+	public String toString() {
+		return "Person [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + "]";
+	}
 }
