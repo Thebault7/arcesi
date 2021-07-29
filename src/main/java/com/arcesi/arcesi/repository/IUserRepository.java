ed1e259936332f6d08f09f8e7ccf55bf19b99d16
Thebault7 <frederic.thebault35@gmail.com>
2021-07-29 02:41:01 +0200

    fifth commit

--- src/main/java/com/arcesi/arcesi/repository/IUserRepository.java
+++ src/main/java/com/arcesi/arcesi/repository/IUserRepository.java
@@ -3,9 +3,11 @@ package com.arcesi.arcesi.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.arcesi.arcesi.model.User;
 
+@Repository
 public interface IUserRepository extends JpaRepository<User, Long> {
 
 	public User saveAndFlush(User u);
